9a8a5bf01d533d00eda89b9c05771e0559697c5c
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-03-06 11:27:15 +0000

    Minor improvements; Move inference to separate script

--- map_model.py
+++ map_model.py
@@ -1,18 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-from matplotlib.backends.backend_pdf import PdfPages
+from pathlib import Path
 from copy import deepcopy
 from graphnet.data.constants import FEATURES, TRUTH
 from icecube_utils import make_dataloader
 
 from icecube_utils import (
     load_pretrained_model,
-    inference, 
     make_dataloader,
     map_model,
-    convert_to_3d,
-    calculate_angular_error
 )
 from graphnet.training.labels import Direction
 from parameters import get_parser
@@ -24,13 +18,11 @@ truth = TRUTH.KAGGLE
 
 def get_args():
     parser = get_parser()
-    parser.add_argument('--pdf-save-path', type=str, default='mapping_results.pdf')
-    args = parser.parse_args('--gpu-id 1 --model-name mlpnet --n-epochs 10 --save-result-file sample.csv \
-    --sweep-name exp_sample --exact --correction --ground-metric euclidean --weight-stats \
-    --activation-histograms --activation-mode raw --geom-ensemble-type acts --sweep-id 21 \
-    --act-num-samples 200 --ground-metric-normalize none --activation-seed 21 \
-    --prelu-acts --recheck-acc --load-models ./mnist_models --ckpt-type final \
-    --past-correction --not-squared --dist-normalize --print-distances --to-download'.split())
+    parser.add_argument('--size-multiplier', type=int, default=1)
+    parser.add_argument('--from-model-state-dict-path', type=Path, required=True)
+    parser.add_argument('--to-model-state-dict-path', type=Path, required=True)
+    parser.add_argument('--mapped-model-save-dir', type=Path, required=True)
+    args = parser.parse_args()
 
     args.gpu_id = 0
     args.proper_marginals = True
@@ -46,72 +38,47 @@ def get_args():
 def main(args):
     models = {}
 
-    # Load pretrained models
-    config_small = deepcopy(config)
-    config_small['dynedge']['dynedge_layer_sizes'] = [(128, 256), (336, 256), (336, 256), (336, 256)]
+    config_from = deepcopy(config)
+    config_from['dynedge']['dynedge_layer_sizes'] = [(128, 256), (336, 256), (336, 256), (336, 256)]
 
-    models['small'] = load_pretrained_model(
-        config=config_small, 
-        state_dict_path='/workspace/icecube/weights/dynedge_pretrained_batch_1_to_50/state_dict.pth'
+    models['from'] = load_pretrained_model(
+        config=config_from, 
+        state_dict_path=str(args.from_model_state_dict_path)
     )
 
-    config_large = deepcopy(config)
-    config_large['dynedge']['dynedge_layer_sizes'] = [(128 * 2, 256 * 2), (336 * 2, 256 * 2), (336 * 2, 256 * 2), (336 * 2, 256 * 2)]
+    config_to = deepcopy(config)
+    config_to['dynedge']['dynedge_layer_sizes'] = [
+        (x * args.size_multiplier, y * args.size_multiplier) 
+        for x, y in [(128, 256), (336, 256), (336, 256), (336, 256)]
+    ]
 
-    models['large_bad'] = load_pretrained_model(
-        config=config_large, 
-        state_dict_path='/workspace/icecube/weights/dynedge_pretrained_large_0_epochs_1/state_dict.pth'
+    models['to'] = load_pretrained_model(
+        config=config_to, 
+        state_dict_path=str(args.to_model_state_dict_path)
     )
 
-    # Map small model to large model
-    train_dataloader = make_dataloader(db=config_small['path'],
+    # Map from model to to model
+    train_dataloader = make_dataloader(db=config_from['path'],
         selection=None, # Entire database
-        pulsemaps=config_small['pulsemap'],
+        pulsemaps=config_from['pulsemap'],
         features=features,
         truth=truth,
         batch_size=args.act_num_samples,
-        num_workers=config_small['num_workers'],
+        num_workers=config_from['num_workers'],
         shuffle=False,
         labels={'direction': Direction()},
-        index_column=config_small['index_column'],
-        truth_table=config_small['truth_table'],
+        index_column=config_from['index_column'],
+        truth_table=config_from['truth_table'],
     )
 
-    models['small_mapped'] = map_model(
+    models['mapped'] = map_model(
         args,
-        models['small'], 
-        models['large_bad'], 
+        models['from'], 
+        models['to'], 
         train_dataloader, 
         args.act_num_samples
     )
-    torch.cuda.empty_cache()
-
-    # Inference
-    results = {}
-    for model_name, model in models.items():
-        results[model_name] = calculate_angular_error(
-            convert_to_3d(
-                inference(
-                    model, 
-                    config_large
-                )
-            )
-        )
-        torch.cuda.empty_cache()
-
-    # Plot results
-    with PdfPages(args.pdf_save_path) as pdf:
-        fig = plt.figure(figsize = (6,6))
-        for name, result in results.items():
-            plt.hist(result['angular_error'], 
-                    bins = np.arange(0,np.pi*2, 0.05), 
-                    histtype = 'step', 
-                    label = f'{name} mean AE: {np.round(result["angular_error"].mean(),2)}')
-            plt.xlabel('Angular Error [rad.]', size = 15)
-            plt.ylabel('Counts', size = 15)
-        plt.title(f'{name}, Angular Error Distribution (Batch 51)', size = 15)
-        plt.legend(frameon=False, fontsize=15)
-        pdf.savefig(fig)
+    models['mapped'].save_state_dict(str(args.mapped_model_save_dir / 'state_dict.pth'))
 
 
 if __name__ == '__main__':
