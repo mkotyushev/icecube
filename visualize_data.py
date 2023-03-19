65fc869a4bcc960ef50a6263762953efb0ac423a
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-03-19 11:10:01 +0000

    Update data visualization

--- visualize_data.py
+++ visualize_data.py
@@ -1,5 +1,5 @@
 import sqlite3
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -15,11 +15,14 @@ df = df.sample(100)
 
 # Load sensors geometry
 df_sensor_geometry = pd.read_csv('data/dataset/sensor_geometry.csv')
-df_pulse = None
+
+with sqlite3.connect(database_path) as conn:
+    pulse_query = f'SELECT * FROM pulse_table WHERE event_id = {df.iloc[0]["event_id"]}'
+    df_pulse = pd.read_sql(pulse_query, conn)
 
 # Create static figure of sensors geometry
 # with blank trace for data
-data = [
+original_data = [
     go.Scatter3d(
         visible = True,
         x = df_sensor_geometry.x,
@@ -28,16 +31,19 @@ data = [
         mode='markers',
         marker=dict(
             size=1,
-        )
+        ),
+        uirevision='constant',
     ),
     go.Scatter3d(
         visible = True,
         mode='markers',
+        uirevision='constant',
     )
 ]
 
-fig = go.Figure(data=data)
+fig = go.Figure(data=original_data)
 fig.layout.scene.camera.projection.type = "orthographic"
+fig.layout.uirevision = 'constant'
 
 app = Dash(__name__)
 app.layout = html.Div([
@@ -52,43 +58,99 @@ app.layout = html.Div([
     ),
     dcc.Slider(
         id='time-slider'
-    )
+    ),
+    dcc.Interval(id="animate", disabled=True),
+    html.Button("Play/Stop", id="play"),
 ])
 
 
 @app.callback(
+    Output('time-slider', 'value'),
     Output('time-slider', 'min'),
     Output('time-slider', 'max'),
     Output('graph-with-slider', 'figure'),
     Input('event_id-slider', 'value'))
-def update_figure(event_id):
-    global database_path, fig, df_pulse
+def update_figure_on_event_id_change(event_id):
+    global database_path, df_pulse
     with sqlite3.connect(database_path) as conn:
         pulse_query = f'SELECT * FROM pulse_table WHERE event_id = {event_id}'
         df_pulse = pd.read_sql(pulse_query, conn)
 
-    fig.data[1]['x'] = df_pulse.x
-    fig.data[1]['y'] = df_pulse.y
-    fig.data[1]['z'] = df_pulse.z
-    fig.data[1]['marker']['size'] = df_pulse.charge * 10
+    data = [
+        original_data[0],
+        go.Scatter3d(
+            visible = True,
+            x = df_pulse.x,
+            y = df_pulse.y,
+            z = df_pulse.z,
+            mode='markers',
+            marker={
+                'size': df_pulse.charge * 10,
+                'opacity': 0.5,
+            },
+            uirevision='constant',
+        ),
+    ]
 
-    return df_pulse.time.min(), df_pulse.time.max(), fig
+    return df_pulse.time.max(), df_pulse.time.min(), df_pulse.time.max(), {
+        'data': data,
+        'layout': {
+            'uirevision': 'constant',
+        },
+    }
 
 
 @app.callback(
     Output('graph-with-slider', 'figure', allow_duplicate=True),
     Input('time-slider', 'value'), prevent_initial_call=True)
-def update_figure(time):
-    global database_path, fig, df_pulse
+def update_figure_on_time_change(time):
+    global df_pulse
 
-    if df_pulse is not None:
-        mask = df_pulse.time <= time
-        fig.data[1]['x'] = df_pulse.x[mask]
-        fig.data[1]['y'] = df_pulse.y[mask]
-        fig.data[1]['z'] = df_pulse.z[mask]
-        fig.data[1]['marker']['size'] = df_pulse.charge[mask] * 10
+    mask = df_pulse.time <= time
+    data = [
+        original_data[0],
+        go.Scatter3d(
+            visible = True,
+            x = df_pulse.x[mask],
+            y = df_pulse.y[mask],
+            z = df_pulse.z[mask],
+            mode='markers',
+            marker={
+                'size': df_pulse.charge[mask] * 10,
+                'opacity': 0.5,
+            },
+            uirevision='constant',
+        ),
+    ]
 
-    return fig
+    return {
+        'data': data,
+        'layout': {
+            'uirevision': 'constant',
+        },
+    }
+
+
+@app.callback(
+    Output("time-slider", "value", allow_duplicate=True),
+    Input('animate', 'n_intervals'),
+    prevent_initial_call=True,
+)
+def update_output(n_intervals):
+    global df_pulse
+    selected_value = df_pulse.iloc[(n_intervals % len(df_pulse))]['time']
+    return selected_value
+
+
+@app.callback(
+    Output("animate", "disabled"),
+    Input("play", "n_clicks"),
+    Input("animate", "disabled"),
+)
+def toggle(n_clicks, disabled):
+    if n_clicks:
+        return not disabled
+    return disabled
 
 
 if __name__ == '__main__':
