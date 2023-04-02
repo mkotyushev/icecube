bd49474061770e5668fb93cea2dd6a81eb590351
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-02 17:32:50 +0000

    Increase precision for simplex

--- train_large.py
+++ train_large.py
@@ -197,6 +197,11 @@ if __name__ == '__main__':
         config['fit']['val_check_interval']
     )
 
+    # Simplex requires higher precision
+    if args.train_mode == 'simplex':
+        config['fit']['precision'] = 64
+        torch.set_float32_matmul_precision('highest')
+
     # Replace max_epochs with iterating files max_epochs times
     # to apply files shuffilg for parallel_parquet for each epoch
     # and change related parameters accordingly
