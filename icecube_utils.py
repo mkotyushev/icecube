d02ee66cc039d4676d1852bae593134757cb2565
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-04-11 10:14:03 +0000

    Add metrics for direection & s2 tasks

--- icecube_utils.py
+++ icecube_utils.py
@@ -194,6 +194,139 @@ def make_scheduler_kwargs(config, train_dataloader):
     return scheduler_kwargs
 
 
+def xyz_to_angles(x, y, z):
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    azimuth = np.arctan2(y, x)
+    azimuth[azimuth < 0] += 2 * np.pi
+    zenith = np.arccos(z / r)
+    return azimuth, zenith
+
+
+def angular_dist_score(az_true, zen_true, az_pred, zen_pred):
+    '''
+    calculate the MAE of the angular distance between two directions.
+    The two vectors are first converted to cartesian unit vectors,
+    and then their scalar product is computed, which is equal to
+    the cosine of the angle between the two vectors. The inverse 
+    cosine (arccos) thereof is then the angle between the two input vectors
+    
+    Parameters:
+    -----------
+    
+    az_true : float (or array thereof)
+        true azimuth value(s) in radian
+    zen_true : float (or array thereof)
+        true zenith value(s) in radian
+    az_pred : float (or array thereof)
+        predicted azimuth value(s) in radian
+    zen_pred : float (or array thereof)
+        predicted zenith value(s) in radian
+    
+    Returns:
+    --------
+    
+    dist : float
+        mean over the angular distance(s) in radian
+    '''
+    
+    if not (np.all(np.isfinite(az_true)) and
+            np.all(np.isfinite(zen_true)) and
+            np.all(np.isfinite(az_pred)) and
+            np.all(np.isfinite(zen_pred))):
+        raise ValueError("All arguments must be finite")
+    
+    # pre-compute all sine and cosine values
+    sa1 = np.sin(az_true)
+    ca1 = np.cos(az_true)
+    sz1 = np.sin(zen_true)
+    cz1 = np.cos(zen_true)
+    
+    sa2 = np.sin(az_pred)
+    ca2 = np.cos(az_pred)
+    sz2 = np.sin(zen_pred)
+    cz2 = np.cos(zen_pred)
+    
+    # scalar product of the two cartesian vectors (x = sz*ca, y = sz*sa, z = cz)
+    scalar_prod = sz1*sz2*(ca1*ca2 + sa1*sa2) + (cz1*cz2)
+    
+    # scalar product of two unit vectors is always between -1 and 1, this is against nummerical instability
+    # that might otherwise occure from the finite precision of the sine and cosine functions
+    scalar_prod =  np.clip(scalar_prod, -1, 1)
+    
+    # convert back to an angle (in radian)
+    return np.arccos(scalar_prod)
+
+
+def angular_error_task_direction(preds: List[Tensor], data: Data) -> Tensor:
+    """
+    Calculates angular error for direction task.
+
+    Args:
+        preds: Tasks predictions len(preds) == 1, 
+            preds[0] -- direction task predictions, 
+            preds[0].shape (N, 4) -- (x, y, z, kappa)
+        data: Batch data
+    
+    Returns:
+        angular error array, shape (N,)
+    """
+    true_azimuth, true_zenith = data['azimuth'].cpu().numpy(), data['zenith'].cpu().numpy()
+
+    pred_x = preds[0][:, 0].cpu().numpy()
+    pred_y = preds[0][:, 1].cpu().numpy()
+    pred_z = preds[0][:, 2].cpu().numpy()
+    pred_azimuth, pred_zenith = xyz_to_angles(pred_x, pred_y, pred_z)
+
+    angular_error = angular_dist_score(true_azimuth, true_zenith, pred_azimuth, pred_zenith)
+    return torch.from_numpy(angular_error)
+
+
+def angular_error_task_s2(preds: List[Tensor], data: Data) -> Tensor:
+    """
+    Calculates angular error for s2 task.
+
+    Args:
+        preds: Tasks predictions len(preds) == 2, 
+            preds[0] -- s2 abs task predictions, 
+            preds[0].shape (N, 3) -- (x, y, z)
+            preds[0] in [0, 1]
+
+            preds[1] -- s2 sign task predictions,
+            preds[1].shape (N, 8) -- (sign class logits)
+
+            Sign class:
+                0 -> -1, -1, -1	
+                1 -> -1, -1,  1
+                2 -> -1,  1, -1
+                3 -> -1,  1,  1
+                4 ->  1, -1, -1
+                5 ->  1, -1,  1
+                6 ->  1,  1, -1
+                7 ->  1,  1,  1
+        
+        data: Batch data
+    
+    Returns:
+        angular error array, shape (N,)
+    """
+    true_azimuth, true_zenith = data['azimuth'].cpu().numpy(), data['zenith'].cpu().numpy()
+
+    pred_x_abs = preds[0][:, 0].cpu().numpy()
+    pred_y_abs = preds[0][:, 1].cpu().numpy()
+    pred_z_abs = preds[0][:, 2].cpu().numpy()
+
+    pred_sign_class = preds[1].cpu().numpy().argmax(axis=1)
+    pred_x, pred_y, pred_z = pred_x_abs, pred_y_abs, pred_z_abs
+    pred_x[pred_sign_class & 0b100] *= -1
+    pred_y[pred_sign_class & 0b010] *= -1
+    pred_z[pred_sign_class & 0b001] *= -1
+
+    pred_azimuth, pred_zenith = xyz_to_angles(pred_x, pred_y, pred_z)
+
+    angular_error = angular_dist_score(true_azimuth, true_zenith, pred_azimuth, pred_zenith)
+    return torch.from_numpy(angular_error)
+
+
 def build_model(
     config: Dict[str, Any], 
     train_dataloader: Any, 
@@ -211,7 +344,7 @@ def build_model(
         **config['dynedge']
     )
 
-    tasks = []
+    tasks, metric_fn = [], None
     if config["target"] == 'direction':
         task = DirectionReconstructionWithKappa(
             hidden_size=gnn.nb_outputs,
@@ -227,6 +360,7 @@ def build_model(
                               config["target"] + "_z", 
                               config["target"] + "_kappa" ]
         additional_attributes = ['zenith', 'azimuth', 'event_id']
+        metric_fn = angular_error_task_direction
     elif config["target"] == 's2':
         task = S2AbsDirectionReconstruction(
             hidden_size=gnn.nb_outputs,
@@ -260,7 +394,7 @@ def build_model(
             config["target"] + "sign_7_logit",
         ]
         additional_attributes = ['zenith', 'azimuth', 'event_id']
-
+        metric_fn = angular_error_task_s2
     elif config["target"] == 'angles_legacy':
         task = ZenithReconstructionWithKappa(
             hidden_size=gnn.nb_outputs,
@@ -458,6 +592,7 @@ def build_model(
         scheduler_config={
             "interval": "step",
         },
+        metric_fn=metric_fn,
         **config["model_kwargs"],
     )
     model.prediction_columns = prediction_columns
@@ -1344,18 +1479,6 @@ def angles_to_xyz(azimuth, zenith):
     z = np.cos(zenith)
     return x, y, z
 
-
-def xyz_to_angles(x, y, z):
-    norm = np.linalg.norm([x, y, z], ord=2)
-    x, y, z = x / norm, y / norm, z / norm
-
-    azimuth = np.arctan2(y, x)
-    if azimuth < 0:
-        azimuth += 2 * np.pi
-    
-    zenith = np.arccos(z)
-    
-    return azimuth, zenith
 
 
 class FlipTimeTransform(RandomTransform):
